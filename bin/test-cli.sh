2925a9a8db8702d7ccccceb5d57ed7db4a3468c3
Soatok Dreamseeker <soatok.dhole@gmail.com>
2020-08-07 20:45:44 -0400

    Fix CLI test command

--- bin/test-cli.sh
+++ bin/test-cli.sh
@@ -2,4 +2,4 @@
 
 basedir=$( dirname $( readlink -f ${BASH_SOURCE[0]} ) )
 
-php "${basedir}/minisign" -Vm "${usage.txt}" -P 9Gxq9/iRbNZeDzpF4SOwgwqTUt4v3A8gsPO9LktyQRI=
+php "${basedir}/minisign" -Vm "${basedir}/usage.txt" -P 9Gxq9/iRbNZeDzpF4SOwgwqTUt4v3A8gsPO9LktyQRI=
